e67fe0c16977535b48ee13496ce68ef60c001e4d
Triceratroll <adrianmf06@gmail.com>
2023-01-31 17:10:55 +0100

    Mod folder behaviour

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,11 +1,16 @@
 import os
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
-from datetime import datetime
-from report_1 import generate_order_prices
-from report_2 import generate_product_customers
-from report_3 import generate_customer_ranking
-from utils import clearDirectory
+from flaskr.report_1 import generate_order_prices
+from flaskr.report_2 import generate_product_customers
+from flaskr.report_3 import generate_customer_ranking
+from flaskr.utils import clearDirectory
+
+from pathlib import Path
+
+# get the resources folder in the tests folder
+input = Path(__file__).parent / "input"
+output = Path(__file__).parent / "output"
 
 
 ALLOWED_EXTENSIONS = {'csv'}
@@ -24,19 +29,20 @@ def create_app():
     @app.route('/upload_report_1', methods=['GET', 'POST'])
     def upload_report_1():
         
-        clearDirectory('input')
-        clearDirectory('output')
+        clearDirectory(input)
+        clearDirectory(output)
         
         if request.method == 'POST':
 
             files = request.files.getlist("files")
             for file in files:
-                if file and allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    save_location =  os.path.join('input', filename)
+                filename = os.path.basename(file.filename) 
+                if file and allowed_file(filename):
+                    filename = secure_filename(filename)
+                    save_location =  os.path.join(input, filename)
                     file.save(save_location)
             
-            output_file = generate_order_prices('input', 'output')
+            output_file = generate_order_prices(input, output)
             return redirect(url_for('download_file', filename=output_file))  
 
         return render_template('upload_report_1.html')
@@ -45,19 +51,20 @@ def create_app():
     @app.route('/upload_report_2', methods=['GET', 'POST'])
     def upload_report_2():
         
-        clearDirectory('input')
-        clearDirectory('output')
+        clearDirectory(input)
+        clearDirectory(input)
         
         if request.method == 'POST':
 
             files = request.files.getlist("files")
             for file in files:
-                if file and allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    save_location =  os.path.join('input', filename)
+                filename = os.path.basename(file.filename) 
+                if file and allowed_file(filename):
+                    filename = secure_filename(filename)
+                    save_location =  os.path.join(input, filename)
                     file.save(save_location)
             
-            output_file = generate_product_customers('input', 'output')
+            output_file = generate_product_customers(input, output)
             return redirect(url_for('download_file', filename=output_file))  
 
         return render_template('upload_report_2.html')
@@ -66,20 +73,21 @@ def create_app():
     @app.route('/upload_report_3', methods=['GET', 'POST'])
     def upload_report_3():
         
-        clearDirectory('input')
-        clearDirectory('output')
+        clearDirectory(input)
+        clearDirectory(output)
         
         if request.method == 'POST':
 
             files = request.files.getlist("files")
             for file in files:
-                if file and allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    save_location =  os.path.join('input', filename)
+                filename = os.path.basename(file.filename)
+                if file and allowed_file(filename):
+                    filename = secure_filename(filename)
+                    save_location =  os.path.join(input, filename)
                     file.save(save_location)
             
             
-            output_file = generate_customer_ranking('input', 'output')
+            output_file = generate_customer_ranking(input, output)
             return redirect(url_for('download_file', filename=output_file)) 
 
         return render_template('upload_report_3.html')
@@ -87,7 +95,7 @@ def create_app():
 
     @app.route('/download/<filename>')
     def download_file(filename):
-        return send_from_directory('output', filename)  
+        return send_from_directory(output, filename)  
 
 
     return app 
